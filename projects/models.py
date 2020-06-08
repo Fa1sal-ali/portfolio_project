089ae3eb5aca239f57942e1de65381786f3f57f1
Faisal Ali <fzl.ali33@gmail.com>
2020-06-08 23:59:17 +0530

    Projects App up and running

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Input(models.Model):
+    image = models.ImageField(upload_to="images/")
+    title = models.CharField(max_length=50)
+    summary = models.CharField(max_length=200)
